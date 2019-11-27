cc0d793fc592a961a61089692dc52053ff41f42a
SimonFangChicago <47611549+SimonFangChicago@users.noreply.github.com>
2019-11-26 18:16:46 -0600

    test
    test

--- finalproject/businesscards/src/app/web-cam/web-cam.component.ts
+++ finalproject/businesscards/src/app/web-cam/web-cam.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {BusinessCardService} from '../services/business-card.service';
 import {BusniessCardModel} from '../model/busniess-card-model';
 import { Router } from  "@angular/router";
+import domtoimage from 'dom-to-image';
 
 @Component({
   selector: 'app-web-cam',
@@ -18,6 +19,8 @@ export class WebCamComponent implements OnInit {
     public canvas: ElementRef;
 
     public capture_image: any;
+
+    private base64String : string;
 
     public constructor(private http : HttpClient,private cardsService:BusinessCardService,private router:Router) {
         this.capture_image = '';
@@ -37,21 +40,71 @@ export class WebCamComponent implements OnInit {
     public capture() {
         var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
         //this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
-		this.capture_image = 'https://www.jje.sg/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/s/ssbw-01.png';
-
+		this.capture_image = this.canvas.nativeElement.toDataURL("image/png");//'https://www.jje.sg/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/s/ssbw-01.png';
+		this.convertToBase64();
         this.OCR();
     }
 
     public OCR()
     {
-    	const request: any = {
+    	/*const request: any = {
 		 'requests': [
 		 {
 		 'image': {
 		 'source': {
 		 'imageUri':
-		'https://www.jje.sg/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/s/ssbw-01.png',
+		this.capture_image,
 		 },
+		 },
+		 'features': [
+		 {
+		 'type': 'TEXT_DETECTION',
+		 'maxResults': 1,
+		 }
+		 ]
+		 }
+		 ]
+		 };
+		 const url =
+		'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBby6pLqEOiul44__X2MGZsNXF9SzzlQEE';
+		 this.http.post(
+		 url,
+		 request
+		 ).subscribe( (results: any) => {
+		 //console.log('RESULTS RESULTS RESULTS');
+		 console.log(results);
+		 console.log(results.responses["0"].fullTextAnnotation);
+
+		 var _name = results.responses["0"].fullTextAnnotation.text;
+		 var _address = results.responses["0"].fullTextAnnotation.text;
+		 var _email = results.responses["0"].fullTextAnnotation.text;
+		 var _number = results.responses["0"].fullTextAnnotation.text;
+
+		 
+
+		 var newCard = {
+		 	firstName : _name,
+		 	lastName : _name,
+		 	email : _email,
+		 	phoneNumber : _number,
+		 	additionalInfo : ''
+		 }
+
+		 console.log(newCard);
+		 //console.log('RESULTS RESULTS RESULTS');
+
+		this.cardsService.add(newCard);
+
+
+  		this.router.navigate(['busniessCards']);
+
+		 });*/
+
+		 const request: any = {
+		 'requests': [
+		 {
+		 'image': {
+		 	"content": this.base64String
 		 },
 		 'features': [
 		 {
@@ -98,4 +151,24 @@ export class WebCamComponent implements OnInit {
 		 });
 
     }
+
+    convertToBase64() {
+
+    const imgNode = this.canvas.nativeElement;
+
+      console.log('SELECTED IMAGE');
+      console.log(imgNode);
+      console.log('SELECTED IMAGE');
+      domtoimage.toPng(imgNode)
+      .then( (dataUrl: string) => {
+        console.log('SELECTED IMAGE 2');
+        console.log(dataUrl);
+        this.base64String = dataUrl;
+        console.log('SELECTED IMAGE 2');
+      }).catch( (e: any) => {
+        console.log('SELECTED IMAGE BASE64 SOMETHING WENT WRONG');
+        console.log(e);
+      });
+
+  }
 }
